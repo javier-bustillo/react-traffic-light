57f57609d7f9a4355cdf0dbcd06bd3b536db6807
javier-bustillo <jjavier.bustillo@gmail.com>
2022-06-13 19:18:59 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,55 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("btn-secondary");
+	const [color1, setColor1] = useState("btn-secondary");
+	const [color2, setColor2] = useState("btn-secondary");
+
+	function changeColor() {
+		setColor("btn-success");
+		if (color1 === "btn-warning" || color2 === "btn-danger") {
+			setColor1("btn-secondary");
+			setColor2("btn-secondary");
+		}
+	}
+	function changeColor1() {
+		setColor1("btn-warning");
+		if (color === "btn-success" || color2 === "btn-danger") {
+			setColor("btn-secondary");
+			setColor2("btn-secondary");
+		}
+	}
+
+	function changeColor2() {
+		setColor2("btn-danger");
+		if (color === "btn-success" || color1 === "btn-warning") {
+			setColor("btn-secondary");
+			setColor1("btn-secondary");
+		}
+	}
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex flex-column">
+			<h1 className="text-center my-5">Traffic Light</h1>
+			<button
+				type="button"
+				className={`btn ${color} my-2`}
+				onClick={changeColor}>
+				Boom! Green Light!
+			</button>
+			<button
+				type="button"
+				className={`btn ${color1} my-2`}
+				onClick={changeColor1}>
+				Wow! Yellow Light!
+			</button>
+			<button
+				type="button"
+				className={`btn ${color2} my-2`}
+				onClick={changeColor2}>
+				Awesome! Red Light!
+			</button>
 		</div>
 	);
 };
